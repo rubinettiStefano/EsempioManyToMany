82c31bc0b58e6638c3176e8433c17f74dd0d65bc
StefanoR <rubinetti.stefano@gmail.com>
2024-11-21 11:27:23 +0100

    davai

--- src/main/java/com/generation/controller/ApplicationController.java
+++ src/main/java/com/generation/controller/ApplicationController.java
@@ -1,53 +1,13 @@
 package com.generation.controller;
 
 import com.generation.helpers.ControllerHelper;
-import com.generation.model.House;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
 public class ApplicationController
 {
-	@FXML
-	public TextField address,smp,area,type,house_id;
-	@FXML
-	public Text listaCase;
 
 	private ControllerHelper helper = ControllerHelper.getInstance();
 
-	public void refresh()
-	{
-		listaCase.setText(helper.produciListaCase());
-	}
-
-	public void insert()
-	{
-		String address = this.address.getText();
-		double smp = Double.parseDouble(this.smp.getText());
-//		v1
-//		House house = new House();
-//		house.setAddress(address);
-//		house.setSmp(smp);
-//
-//		helper.insertHouse(house);
-
-		//v2
-		helper.insertHouse(address,smp);
-		this.address.clear();
-		this.smp.clear();
-		refresh();
-	}
-
-	public void insertRoom()
-	{
-		String type = this.type.getText();
-		double area = Double.parseDouble(this.area.getText());
-		int house_id = Integer.parseInt(this.house_id.getText());
-
-		helper.insertRoom(type,area,house_id);
-		this.type.clear();
-		this.area.clear();
-		this.house_id.clear();
-		refresh();
-	}
 }
--- src/main/java/com/generation/helpers/ControllerHelper.java
+++ src/main/java/com/generation/helpers/ControllerHelper.java
@@ -1,10 +1,5 @@
 package com.generation.helpers;
 
-import com.generation.model.House;
-import com.generation.model.HouseRepository;
-import com.generation.model.Room;
-import com.generation.model.RoomRepository;
-
 import java.util.List;
 
 public class ControllerHelper
@@ -19,40 +14,4 @@ public class ControllerHelper
 	}
 	private ControllerHelper(){}
 
-	private HouseRepository hRepo = HouseRepository.getInstance();
-	private RoomRepository rRepo = RoomRepository.getInstance();
-
-	public void insertHouse(House house)
-	{
-		hRepo.insertHouse(house);
-	}
-
-	public void insertHouse(String address, double smp)
-	{
-		House house = new House();
-		house.setAddress(address);
-		house.setSmp(smp);
-		hRepo.insertHouse(house);
-	}
-
-	public String produciListaCase()
-	{
-		List<House> all = hRepo.findAllHouses();
-
-		String res = "";
-
-		for(House house : all)
-			res+=house.getId()+","+house.getAddress()+",price: "+house.calcPrice()+" euro\n";
-		return res;
-	}
-
-	public void insertRoom(String type, double area, int houseId)
-	{
-		Room room = new Room();
-		room.setType(type);
-		room.setArea(area);
-		House toConnect = hRepo.findHouse(houseId);
-		room.setHouse(toConnect);
-		rRepo.insertRoom(room);
-	}
 }
